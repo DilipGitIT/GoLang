b9a7ea6af5063ab12b62f620041589a1606a6fca
DilipGitIT <smilesaheaddkp@gmail.com>
2020-02-16 14:49:34 +0000

    second commit - add

---
+++ add/add.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num1, num2, result1 int
+	num3 := 5
+	fmt.Printf("Enter the values")
+	fmt.Scanf("%d %d", &num1, &num2)
+	result1 = num1 + num2 + num3
+	fmt.Printf("Result= %v", result1)
+}
