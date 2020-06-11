31b2e0ecd300a86aeef694362e34da3cf385d560
DilipGitIT <smilesaheaddkp@gmail.com>
2020-06-11 18:46:38 +0100

    5th change

--- add/add.go
+++ add/add.go
@@ -6,10 +6,10 @@ import (
 
 func main() {
 	var num1, num2, result1 int
-	num3 := 10
+	num3 := 20
 	fmt.Printf("Enter the values")
 	fmt.Scanf("%d %d", &num1, &num2)
 	result1 = num1 + num2 + num3
 	fmt.Printf("Result= %v", result1)
-	
+
 }
